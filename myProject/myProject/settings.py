090e8b54f28bf0996df563f3fd4d3ac73b3c38ea
tharunboda <tharunb978@gmail.com>
2025-11-11 15:54:31 +0530

    error ok

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -77,6 +77,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'PASSWORD':'123456',
+        'HOST':"127.0.0.1",
+        'PORT':"3306",
+        "OPTIONS":{
+            'charset':'utf8mb4',
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 

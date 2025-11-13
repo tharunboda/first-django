88fd714b0a44d3186f5dac461a683cf935a82f60
tharunboda <tharunb978@gmail.com>
2025-11-13 11:48:10 +0530

    sql change

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'myProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': "10000coders",
         'USER':'root',
         'PASSWORD':'123456',

10a43f9b54096406edf7d0131af783a8d3a418e1
tharunboda <tharunb978@gmail.com>
2025-11-13 11:18:36 +0530

    change

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'myProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
-        
+        'NAME': "django",
+        'PASSWORD':'123456',
+        'HOST':"127.0.0.1",
+        'PORT':"3306",
+        "OPTIONS":{
+            'charset':'utf8mb4',
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 

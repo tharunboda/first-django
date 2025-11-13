bf8c183f51ef1cf514e46b9139ca17bdf363ee50
tharunboda <tharunb978@gmail.com>
2025-11-13 11:21:34 +0530

    successful change

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -78,11 +78,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': "django",
+        'USER':'root',
         'PASSWORD':'123456',
         'HOST':"127.0.0.1",
         'PORT':"3306",
         "OPTIONS":{
-            'charset':'utf8mb4',
+            'chrset':'utf8mb4',
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }

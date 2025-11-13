f7e9cd5f210078d94d7c260c10c32ee30e5cf332
tharunboda <tharunb978@gmail.com>
2025-11-13 11:27:45 +0530

    now done

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'HOST':"127.0.0.1",
         'PORT':"3306",
         "OPTIONS":{
-            'chrset':'utf8mb4',
+            'charset':'utf8mb4',
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
